41be1e6a67740284f4b573e874461e367162617d
beemichan <emikoito9@gmail.com>
2021-08-11 11:54:24 -0700

    Forgot pot-switch.cmd

---
+++ Stream/pot-switch.cmd
@@ -0,0 +1,26 @@
+exec O.$EPICS_HOST_ARCH/streamApp $0
+dbLoadDatabase "O.Common/streamApp.dbd"
+streamApp_registerRecordDeviceDriver
+
+
+epicsEnvSet "STREAM_PROTOCOL_PATH","."
+
+drvAsynSerialPortConfigure("baby","/dev/ttyACM0")
+asynSetOption("baby",0,"baud","9600")
+asynSetOption("baby",0,"bits","8")
+asynSetOption("baby",0,"parity","none")
+asynSetOption("baby",0,"stop","1")
+asynSetOption("baby",0,"clocal","Y")
+asynSetOption("baby",0,"crtscts","N")
+
+#drvAsynIPPortConfigure "LO","localhost:40000"
+#vxi11Configure "LO","192.168.1.236",0,0.0,"gpin0"
+
+dbLoadRecords "pot-switch.db"
+
+#log debug output to file
+#streamSetLogfile StreamDebug.log
+
+iocInit
+
+#var streamDebug 1
