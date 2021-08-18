76fc62ed8a480c9913a5cc69b34beb679483c0bf
beemichan <emikoito9@gmail.com>
2021-08-18 16:01:04 -0700

    Updated photodiode EPICS commands for organization.

--- Stream/photo.cmd
+++ Stream/photo.cmd
@@ -6,13 +6,13 @@ streamApp_registerRecordDeviceDriver
 epicsEnvSet "STREAM_PROTOCOL_PATH","."
 
 #setup the hardware
-drvAsynSerialPortConfigure ("BB","/dev/ttyACM0")
-asynSetOption("BB", 0, "baud", "9600")
-asynSetOption("BB", 0, "bits", "8")
-asynSetOption("BB", 0, "parity", "none")
-asynSetOption("BB", 0, "stop", "1")
-asynSetOption("BB", 0, "clocal", "Y")
-asynSetOption("BB", 0, "crtscts", "N")
+drvAsynSerialPortConfigure ("pho","/dev/ttyACM0")
+asynSetOption("pho", 0, "baud", "9600")
+asynSetOption("pho", 0, "bits", "8")
+asynSetOption("pho", 0, "parity", "none")
+asynSetOption("pho", 0, "stop", "1")
+asynSetOption("pho", 0, "clocal", "Y")
+asynSetOption("pho", 0, "crtscts", "N")
 
 #drvAsynIPPortConfigure "L0", "localhost:40000"
 # vxi11Configure "L0", "192.168.1.24",0,0.0,"gpin0"
