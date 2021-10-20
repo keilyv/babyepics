a8c23fb2433304524ccdb400a74753bd6fd633e1
Scott Feister <sfeister@gmail.com>
2021-10-20 14:58:04 -0700

    Not yet tested, but timing generator is nearly complete with its serial command list

--- Arduino/PulseGenerator/PulseGenerator.ino
+++ Arduino/PulseGenerator/PulseGenerator.ino
@@ -1,18 +1,18 @@
 /*
 Generates 3.3 V TTL signals at a fixed repetition rate
 
-Outputs across four channels, each with its own delay time
+Outputs signals across eight channels, each with its own delay time
 
-Following timer instructions at: https://github.com/contrem/arduino-timer
+References:
+ 1. Following timer instructions at: https://github.com/contrem/arduino-timer
+ 2. Following Vrekrer SCPI Parser examples, e.g. at https://github.com/Vrekrer/Vrekrer_scpi_parser/blob/master/examples/Numeric_suffixes/Numeric_suffixes.ino
 
 */
 
 #include "Arduino.h"
 #include <math.h>
 #include <arduino-timer.h>
-
-Timer<10, micros, int> timer1; // TTL pulse timer with 10 task slots, microsecond resolution, and handler argument type int
-Timer<1, millis> timer2; // rep-rate timer with 1 task slot, millisecond resolution, and void-type handler
+#include "Vrekrer_scpi_parser.h"
 
 #define TRIGOUT 3 // Outputs at start of trigger event
 #define NCHAN 8 // Total number independently delayed output signals
@@ -25,12 +25,20 @@ Timer<1, millis> timer2; // rep-rate timer with 1 task slot, millisecond resolut
 #define CH6 10
 #define CH7 11
 
+/* Serial communication initialization */
+SCPI_Parser my_instrument;
+
+/* Timing initialization */
+Timer<10, micros, int> timer1; // TTL pulse timer with 10 task slots, microsecond resolution, and handler argument type int
+Timer<1, millis> timer2; // rep-rate timer with 1 task slot, millisecond resolution, and void-type handler
+
 int chanPins[NCHAN]; // Pins for each channel
 int delays[NCHAN]; // Millisecond delay times for each channel
 
 int pulseTime = 3000; // TTL pulse duration in microseconds
 unsigned long t0;
 
+/* Timing functions */
 bool TTLStop(int chanPin) {
     // Bring this pulse output back to LOW
     digitalWrite(chanPin, LOW);
@@ -65,13 +73,67 @@ void updateRepRate(double repRate){
   timer2.cancel();
   int repTime = round(1000 / repRate);
   timer2.every(repTime, trigStart); // Start a timer for this channel
-  Serial.print("Hello!");
-  Serial.print(repTime);
 }
 
-void setup() {
-  Serial.begin(9600);
+/* Serial communication functions */
+void identify(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  interface.println(F("Sidekick EPICS,Arduino TTL Pulse Generator,#00,v0.1"));
+}
+
+
+void getRepRate(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  interface.println(repRate)
+}
+
+void setRepRate(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  String first_parameter = String(parameters.First());
+  updateRepRate(first_parameter.toDouble())
+}
+
+void getDelay(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  // This function is modified from the Vrekrer examples: https://github.com/Vrekrer/Vrekrer_scpi_parser/blob/master/examples/Numeric_suffixes/Numeric_suffixes.ino
   
+  //Get the numeric suffix/index (if any) from the commands
+  String header = String(commands.Last());
+  header.toUpperCase();
+  int suffix = -1;
+  sscanf(header.c_str(),"%*[CHANNEL]%u", &suffix);
+
+  //If the suffix is valid, print the channel's value to the interface
+  if ( (suffix >= 0) && (suffix < NCHAN) ) {
+      interface.println(delays[suffix]);
+  }
+}
+
+void setDelay(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  // This function is modified from the Vrekrer examples: https://github.com/Vrekrer/Vrekrer_scpi_parser/blob/master/examples/Numeric_suffixes/Numeric_suffixes.ino
+  
+  //Get the numeric suffix/index (if any) from the commands
+  String header = String(commands.Last());
+  header.toUpperCase();
+  int suffix = -1;
+  sscanf(header.c_str(),"%*[CHANNEL]%u", &suffix);
+
+  //If the suffix is valid,
+  //use the first parameter (if valid) to set the channel delay
+  String first_parameter = String(parameters.First());
+  if ( (suffix >= 0) && (suffix < NCHAN) ) {
+      delays[suffix] = first_parameter.toInt();
+  }
+}
+
+
+void setup() {
+  /* Serial communication setup */
+  my_instrument.RegisterCommand(F("*IDN?"), &identify);
+  my_instrument.RegisterCommand(F("REPrate?"), &getRepRate);
+  my_instrument.RegisterCommand(F("REPrate"), &setRepRate);
+  my_instrument.SetCommandTreeBase(F("DELay:"));
+  my_instrument.RegisterCommand(F("CHannel#?"), &getDelay);
+  my_instrument.RegisterCommand(F("CHannel#"), &setDelay);
+  Serial.begin(9600);
+
+  /* Timing setup */
   pinMode(TRIGOUT, OUTPUT); // Declare the TRIGOUT pin as a digital output
   pinMode(CH0, OUTPUT);
   pinMode(CH1, OUTPUT);
@@ -110,4 +172,5 @@ void setup() {
 void loop() {
   timer1.tick();
   timer2.tick();
+  my_instrument.ProcessInput(Serial, "\n");
 }
